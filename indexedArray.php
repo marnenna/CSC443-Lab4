01093028c8fc2b55b301a6c375eedd0f9a70ce94
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-05-17 22:10:07 +0800

    Indexed Arrays

--- indexedArray.php
+++ indexedArray.php
@@ -8,15 +8,13 @@
 </head>
 <body>
 <?php    
-$month = array ('January','February','March','April','May','June','July','August','September','October','November','December');
-$arrlength=count($month);
+$month = array ('January ','February ','March ','April ','May ','June ','July ','August ','September ','October ','November ','December ');
+$arrlength = count($month);
 
-for($x=0; $x < $arrlength; $x++){
+for($x = 0; $x < $arrlength; $x++){
     echo $month[$x];
-    
+    echo "<br>";
 }
-
-
 ?>
 </body>
 </html>
