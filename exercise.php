8bc5c0c096cd52ea9fd8490a293b3390029ca274
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-05-18 23:53:11 +0800

    Exercise

--- exercise.php
+++ exercise.php
@@ -11,10 +11,8 @@
 
     $monthDays = array("Splorch" => 23, "Sploo" => 28, "Splat" => 2, "Splatt" => 3, "Spleen" => 44, "Splune" => 30, "Spling" => 61, "Slendo" => 61, "Sploctember" => 31, "Splictember" => 31, "Splanet" => 30, "TheRest" => 22);
     foreach($monthDays as $days => $daysnumber){
-
-        echo(max($daysnumber));
-        echo("\n");
-        echo(min($daysnumber));
+        
+        echo("the number of days in the shortest" min($daysnumber));
 
     }
     echo "<br>";
