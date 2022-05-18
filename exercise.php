29c966f31b7b9fcbbd2181126ec9302a0d25dcd8
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-05-19 00:27:55 +0800

    Exercise

--- exercise.php
+++ exercise.php
@@ -8,8 +8,7 @@
 </head>
 <body>
     <?php
-
-    $month = array("Splorch", "Sploo", "Splat", "Splatt", "Spleen", "Splune", "Spling", "Slendo", "Sploctember", "Splictember", "Splanet", "TheRest");    
+    
     $monthDays = array("Splorch" => 23, "Sploo" => 28, "Splat" => 2, "Splatt" => 3, "Spleen" => 44, "Splune" => 30, "Spling" => 61, "Slendo" => 61, "Sploctember" => 31, "Splictember" => 31, "Splanet" => 30, "TheRest" => 22);
 
         echo "The number of days in the shortest month: " . min($monthDays);
