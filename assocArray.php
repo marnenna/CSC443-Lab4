2a122fac52067a9cb8af72635a983f85967683be
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-05-18 23:48:53 +0800

    Exercise

--- assocArray.php
+++ assocArray.php
@@ -9,15 +9,17 @@
 <body>
    
     <?php
-    
+    //answer of question a)
     $monthDays = array ('January' => 31, 'February' => 28, 'March' => 31,'April' => 30,'May' => 31, 'June' => 30, 'July' => 31, 'August' => 31,'September' => 30, 'October' => 31, 'November'=> 30, 'December' => 31);
 
+    //answer of question b)
     foreach($monthDays as $days => $daysnumber){
     echo "$days" . " - " . "$daysnumber";
     echo "<br>";
     }
     echo "<br>";
 
+    //answer of question c)
     foreach($monthDays as $days => $daysnumber30)
     {
         if($daysnumber30 == 30){
---
+++ exercise.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $monthDays = array("Splorch" => 23, "Sploo" => 28, "Splat" => 2, "Splatt" => 3, "Spleen" => 44, "Splune" => 30, "Spling" => 61, "Slendo" => 61, "Sploctember" => 31, "Splictember" => 31, "Splanet" => 30, "TheRest" => 22);
+    foreach($monthDays as $days => $daysnumber){
+
+        echo(max($daysnumber));
+        echo("\n");
+        echo(min($daysnumber));
+
+    }
+    echo "<br>";
+    
+
+    ?>
+</body>
+</html>
