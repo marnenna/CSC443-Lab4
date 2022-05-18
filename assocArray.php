f96ab716b9e3fe28c274f406ad6a73c78edfd913
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-05-18 22:22:05 +0800

    Associative Array

--- assocArray.php
+++ assocArray.php
@@ -10,7 +10,11 @@
    
     <?php
     $monthDays = array ("January" => 31 ,"February" => 30 ,"March" => 31, "April" => 30,"May" => 31 ,"June" => 30 ,"July" => 31,"August" => 31,"September" => 30,"October " => 31,"November" => 30,"December" => 31);
-    print_r($monthDays);
+    foreach( $monthDays as $i => $days){
+        echo "$month[$i] - $days <br>";
+        $i++;
+    }
+    echo "<br>";
     ?>
 </body>
 </html>
